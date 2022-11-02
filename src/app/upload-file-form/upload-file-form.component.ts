2bee02d2e3b978e9e1e92ecded4d4d9660534f1b
Ayush Dhimmar <ayush.dhimmar@1Rintern.local>
2022-11-02 15:42:53 +0530

    feat: folder structure

---
+++ src/app/upload-file-form/upload-file-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-file-form',
+  templateUrl: './upload-file-form.component.html',
+  styleUrls: ['./upload-file-form.component.scss']
+})
+export class UploadFileFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
